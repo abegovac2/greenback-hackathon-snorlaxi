3cdf93c97616c18cec50a236b83c9fecf803fb0c
Admir Ihtijarevic <admirih98@gmail.com>
2021-12-17 19:17:47 +0100

    Added background on home screen

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,23 @@
 import React from 'react'
-import { Text } from 'react-native';
+import { ImageBackground, Text, StyleSheet } from 'react-native';
 
 const HomeScreen = () => {
-    return(
-        <Text>
-            Home Screen
-        </Text>
+    return (
+        <ImageBackground
+            style={styles.imgBackground}
+            resizeMode='cover'
+            source={require('../images/background.jpg')}
+        >
+        </ImageBackground>
     )
 }
 
+const styles = StyleSheet.create({
+    imgBackground: {
+        width: '100%',
+        height: '100%',
+        flex: 1,
+        justifyContent: 'center'
+    }
+})
 export default HomeScreen;
