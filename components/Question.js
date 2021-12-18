1e2b99fa74951869289c187453be25f4d9fc1d63
Admir Ihtijarevic <admirih98@gmail.com>
2021-12-18 11:47:18 +0100

    Fix bg

--- components/Question.js
+++ components/Question.js
@@ -37,7 +37,7 @@ const Question = ({ question, onClickAnswer }) => {
             <Modal
                 animationType="slide"
                 visible={modalVisible}
-                style={{ marginTop: 500 }}
+                style={{ flex: 1 }}
                 onRequestClose={() => {
                     setModalVisible(!modalVisible);
                 }}>
--- screens/QuizScreen.js
+++ screens/QuizScreen.js
@@ -64,7 +64,11 @@ const QuizScreen = ({ navigation }) => {
 
 const styles = StyleSheet.create({
     imgBackground: {
-
+        width: '100%',
+        height: '100%',
+        flex: 1,
+        justifyContent: 'center',
+        flexDirection: 'column'
     }
 })
 export default QuizScreen;
